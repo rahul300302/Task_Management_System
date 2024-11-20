934fa1b170fb954f7fcc3511198250dd3cf328d1
rahul300302 <rahulraw2022@gmail.com>
2024-11-20 16:51:12 +0530

    live tracking

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const http = require("http");
@@ -7,15 +6,14 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const dotenv=require("dotenv")
-dotenv.config('.env')
+const dotenv = require("dotenv");
+dotenv.config(".env");
+
 app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("DB Connection Error:", err));
 
@@ -29,32 +27,43 @@ const vehicleSchema = new mongoose.Schema({
 
 const Vehicle = mongoose.model("Vehicle", vehicleSchema);
 
+// Endpoint to receive and save vehicle location
 app.post("/location", async (req, res) => {
   try {
     const { vehicleId, latitude, longitude } = req.body;
-     await Vehicle.findOneAndUpdate({vehicleId:vehicleId},{ $set:{latitude:latitude,longitude:longitude}},{upsert:true,new:true})
-    setInterval(()=>{
-      io.emit("location-update", { vehicleId, latitude, longitude });
-    },5000)
-    console.log(vehicleId, latitude, longitude);
-    
-    res.status(200).send({ success: true,data:{ vehicleId, latitude, longitude } });
+
+    // Save or update the vehicle location in the database
+    await Vehicle.findOneAndUpdate(
+      { vehicleId: vehicleId },
+      { $set: { latitude: latitude, longitude: longitude } },
+      { upsert: true, new: true }
+    );
+
+    // Emit the updated location to all connected clients
+    io.emit("location-update", { vehicleId, latitude, longitude });
+
+    console.log("Location Updated:", vehicleId, latitude, longitude);
+    res.status(200).send({ success: true, data: { vehicleId, latitude, longitude } });
   } catch (error) {
     console.error("Error saving location:", error);
     res.status(500).send({ success: false, message: "Server error" });
   }
 });
 
+// Serve the frontend
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend.html"));
 });
 
+// Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
 // Start the Server
 server.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
-
-
-                    // map.innerHTML='<iframe width="700" height="300" src="https://maps.google.com/maps?q='+latitude+','+longitude+'&amp;z=15&amp;output=embed"></iframe>'
-                    // map.innerHTML='<iframe width="700" height="300" src=" https://www.google.com/maps/dir/?api=1&origin=12.911955547392678,77.62682971505754&destination=12.913955547392678,77.62882971505754"></iframe>'
